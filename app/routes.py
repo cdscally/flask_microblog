b7c025fbcd9f01d4e0f93627c6a8116cd955779a
cdscally <colin.scally@gmail.com>
2018-02-24 15:38:54 +0000

    adds simple html templates with inheritance

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "hello world!"
+	user = {"username" : "Bob"}
+	posts = [
+		{
+			'author' : {'username':'John'},
+			'body' : 'Beautiful day in London!'
+		},
+		{
+			'author' : {'username':'Sarah'},
+			'body' : "I love the sunshine!"
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts=posts)
